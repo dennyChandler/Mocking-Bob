07e9ec26020f5636d165266020e949c5e99506f9
Denny Chandler - Basement <denny.chandler@dchandler.net>
2017-09-09 00:51:40 -0500

    Added ScrewYou functionality, needs "giphyApiKey" added to config.json

---
+++ ScrewYou/ScrewYou.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GiphyDotNet.Manager;
+using GiphyDotNet.Model.Parameters;
+using Newtonsoft.Json;
+
+namespace sarcasticBob
+{
+    public class ScrewYou
+    {
+        public string Execute()
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+
+            return GetRandomFilePathStream(random);
+        }
+
+        private string GetRandomFilePathStream(Random random)
+        {
+
+            string apiKey;
+            // first, let's load our configuration file
+            var json = "";
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+            {
+                var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+                apiKey = cfgjson.GiphyApiKey;
+
+            }
+            var giphy = new Giphy(apiKey);
+            var rand = new RandomParameter
+            {
+                Tag = "Flip off"
+            };
+            var result = giphy.RandomGif(rand);
+            return result.Result.Data.Url;
+        }
+    }
+
+    public struct ConfigJson
+    {
+        [JsonProperty("giphyApiKey")]
+        public string GiphyApiKey { get; }
+    }
+}
--- sarcasticBob/Commands.cs
+++ sarcasticBob/Commands.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using Microsoft.Win32.SafeHandles;
 
 namespace sarcasticBob.commands
 {
@@ -30,6 +33,14 @@ namespace sarcasticBob.commands
 
             var filePath = utility.CreateSpongeBob(message);
             await ctx.RespondWithFileAsync(filePath, $"{member.Mention}");
+        }
+
+        [Command("fuck"), Description("fuck him"), Aliases("screw")]
+        public async Task Fuck(CommandContext ctx, DiscordMember member)
+        {
+            var daScrew = new ScrewYou();
+            
+            await ctx.RespondAsync($"{member.Mention}", false, new DiscordEmbed{Url = daScrew.Execute() });
         }
     }
 }
